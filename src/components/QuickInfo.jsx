1d18de6d2be236eaa95a0fff94f6d21f86c233c8
anthonyshibitov <anthonyshibitov@gmail.com>
2023-12-01 19:20:53 -0500

    Convert quickinfo to table layout

--- src/components/QuickInfo.jsx
+++ src/components/QuickInfo.jsx
@@ -5,7 +5,7 @@ function QuickInfo(props) {
   const info = props.quickInfo;
   let descriptors;
   const importsPresent = info.importedDlls != undefined;
-  if(importsPresent){
+  if (importsPresent) {
     descriptors = info.importedDlls.ImportDirectoryTable;
   }
   let dllNames;
@@ -53,42 +53,89 @@ function QuickInfo(props) {
         <div className="header-sub-text">
           A quick glance at important attributes.
         </div>
-        <div className="table">
-          <div className="two-column">
-            <div className="two-column-item">File name</div>
-            <div className="two-column-item">{info.name}</div>
-          </div>
-          <div className="two-column">
-            <div className="two-column-item">File size</div>
-            <div className="two-column-item">
+        <table className="second-color">
+          <tr>
+            <td>File name</td>
+            <td>{info.name}</td>
+          </tr>
+          <tr>
+            <td>File size</td>
+            <td>
               {getReadableFileSizeString(info.size)}
-            </div>
-          </div>
-          <div className="two-column">
-            <div className="two-column-item">Platform</div>
-            <div className="two-column-item">
+            </td>
+          </tr>
+          <tr>
+            <td>Platform</td>
+            <td>
               {info.platform == "010B" ? "32-bit" : "64-bit"}
-            </div>
-          </div>
-          <div className="two-column">
-            <div className="two-column-item">Number of sections</div>
-            <div className="two-column-item">{parseInt(info.sectionCount, 16)}</div>
-          </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Number of sections</td>
+            <td>
+              {parseInt(info.sectionCount, 16)}
+            </td>
+          </tr>
           {importsPresent && descriptors != undefined && (
             <>
-              <div className="two-column">
-                <div className="two-column-item">Imported DLLs</div>
-                <div className="two-column-item">{dllNames}</div>
-              </div>
-              <div className="two-column">
-                <div className="two-column-item">Imported Functions</div>
-                <div className="two-column-item">{functionNames}</div>
-              </div>
+              <tr>
+                <td>Imported DLLs</td>
+                <td>{dllNames}</td>
+              </tr>
+              <tr>
+                <td>Imported Functions</td>
+                <td>{functionNames}</td>
+              </tr>
             </>
           )}
-        </div>
+        </table>
       </div>
     </div>
+/*
+      <div id="quick-info-wrapper">
+        <div className="header-wrapper" onClick={() => toggle()}>
+          <div className="header">Quick Information</div>
+        </div>
+        <div>
+          <div className="header-sub-text">
+            A quick glance at important attributes.
+          </div>
+          <div className="table">
+            <div className="two-column">
+              <div className="two-column-item">File name</div>
+              <div className="two-column-item">{info.name}</div>
+            </div>
+            <div className="two-column">
+              <div className="two-column-item">File size</div>
+              <div className="two-column-item">
+                {getReadableFileSizeString(info.size)}
+              </div>
+            </div>
+            <div className="two-column">
+              <div className="two-column-item">Platform</div>
+              <div className="two-column-item">
+                {info.platform == "010B" ? "32-bit" : "64-bit"}
+              </div>
+            </div>
+            <div className="two-column">
+              <div className="two-column-item">Number of sections</div>
+              <div className="two-column-item">{parseInt(info.sectionCount, 16)}</div>
+            </div>
+            {importsPresent && descriptors != undefined && (
+              <>
+                <div className="two-column">
+                  <div className="two-column-item">Imported DLLs</div>
+                  <div className="two-column-item">{dllNames}</div>
+                </div>
+                <div className="two-column">
+                  <div className="two-column-item">Imported Functions</div>
+                  <div className="two-column-item">{functionNames}</div>
+                </div>
+              </>
+            )}
+          </div>
+        </div>
+      </div>*/
   );
 }
 
